76cda5be8f6a0eb5e682d7f3c83e148ec2c11e80
sarasw <117220917+Saraswailm@users.noreply.github.com>
2025-04-28 18:50:18 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,16 +1,17 @@
 
-const url = window.location.href;
+//const url = window.location.href;
 
-fetch("https://guard-extension-api.onrender.com/predict", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ url })
-})
-.then(res => res.json())
-.then(data => {
-  const statusDiv = document.getElementById("status");
-  statusDiv.textContent = data.result === 1 ? "⚠️ Phishing detected!" : "✅ This site appears safe.";
-})
-.catch(() => {
-  document.getElementById("status").textContent = "❌ Error checking URL.";
-});
+//fetch("https://guard-extension-api.onrender.com/predict", {
+  //method: "POST",
+  //headers: { "Content-Type": "application/json" },
+  //body: JSON.stringify({ url })
+//})
+//.then(res => res.json())
+//.then(data => {
+//  const statusDiv = document.getElementById("status");
+//  statusDiv.textContent = data.result === 1 ? "⚠️ Phishing detected!" : "✅ This site appears safe.";
+//})
+//.catch(() => {
+//  document.getElementById("status").textContent = "❌ Error checking URL.";
+//});
+//
